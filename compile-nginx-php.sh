ced5e81873fb86f545e55dab0c8b3bd36aa0b6f3
hrb981027 <hrb@imhrb.com>
2020-06-14 16:52:03 +0800

    修复默认nginx配置文件错误的问题

--- compile-nginx-php.sh
+++ compile-nginx-php.sh
@@ -120,8 +120,8 @@ server {
         fastcgi_pass   unix:/dev/shm/php-fpm-${php_version}.sock;
         fastcgi_index  index.php;
         include        fastcgi_params;
-        fastcgi_param  PATH_INFO $fastcgi_path_info;
-        fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+        fastcgi_param  PATH_INFO \$fastcgi_path_info;
+        fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
     }
 }
 EOF
