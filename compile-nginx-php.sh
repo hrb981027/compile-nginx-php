bb6e2b6d3db23e84d65d7ecbefe81792370a8b4b
hrb981027 <hrb@imhrb.com>
2020-06-18 17:06:44 +0800

    修复默认nginx配置文件错误的问题 && 添加shell可执行权限

--- compile-nginx-php.sh
+++ compile-nginx-php.sh
@@ -82,7 +82,7 @@ http {
     server_tokens off;
 
     log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status $body_bytes_sent "\$http_referer" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
                       '"\$http_user_agent" "\$http_x_forwarded_for"';
 
     access_log  var/log/access.log  main;
